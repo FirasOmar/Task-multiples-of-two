2a1ebefa223bd0d55c04a435d2e861d03fd68ae0
Firas <firas@crmc.co.il>
2023-06-16 20:28:45 +0300

    Add project files.

---
+++ ConsoleApp1/Check.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Check
+    {
+
+        public  bool CheckMod(int n)
+        {
+            if (n % 2 != 0) return false;
+            while ( n/2 > 2)  
+                {
+                int x = n / 2;
+                if (x == 2) return true;
+                else
+                {
+                    if (x % 2 == 1) return false;
+                    n = x;
+                }
+                }
+                return true;
+         
+        }
+            
+            
+           
+            
+       
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+using System.Reflection.Metadata.Ecma335;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Check c = new();
+        var num = Console.ReadLine();
+        int a = Convert.ToInt32(num);
+        
+        bool answer = c.CheckMod(a);
+        Console.WriteLine("number is multiple of two:" +answer);
+        Console.ReadLine();
+    }
+}
